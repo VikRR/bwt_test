333465b4ac1297e84b1be6d7991b444274d91624
VikRR <rubetc.v.i@gmail.com>
2017-03-29 18:06:11 +0300

    add user model

--- app/models/User.php
+++ app/models/User.php
@@ -1,15 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: viktor
- * Date: 27.03.17
- * Time: 20:08
- */
 
 namespace bwttest\app\models;
 
+use bwttest\app\core\Model;
 
-class User
+class User extends Model
 {
+    public $table = 'users';
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function uniqueEmail($data)
+    {
+        $stmt = $this->query("SELECT COUNT(email) as `count` FROM $this->table WHERE email=?", $data);
+
+        $res = $stmt->fetch();
+
+        if($res['count'] == 0){
+
+            return true;
+        }else{
+            throw new \Exception('Email is not unique, please enter another.');
+        }
+    }
+
+    public function insert($params = [])
+    {
+        $stmt = $this->query("INSERT INTO $this->table (first_name, last_name, email, password, male, birthday) VALUES (:first_name, :last_name, :email, :password, :male, :birthday)",
+            $params);
+
+        return $stmt;
+    }
 }
