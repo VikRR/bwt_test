c7d31726a878178f9781fffc7bb57fce2c3155c7
VikRR <rubetc.v.i@gmail.com>
2017-03-29 18:04:47 +0300

    add views index
    form registration

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,3 +1,49 @@
 <div class="row">
-    <h1>Main controller</h1>
-</div>
+    <div class="col-sm-4 col-sm-offset-4 form-box">
+        <div class="form-top text-center">
+            <h3>Register</h3>
+        </div><!-- form-top -->
+        <div class="form-bottom contact-form">
+            <form role="form" action="registration" method="post">
+                <div class="form-group">
+                    <label class="sr-only" for="first_name">Name</label>
+                    <input type="text" name="first_name" placeholder="First name..." class="form-control">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label class="sr-only" for="last_name">Last Name</label>
+                    <input type="text" name="last_name" placeholder="Last Name..." class="form-control">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label class="sr-only" for="email">Email</label>
+                    <input type="text" name="email" class="form-control" placeholder="Email">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label class="sr-only" for="password">Password</label>
+                    <input type="password" name="password" placeholder="Password..." class="form-control">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label class="sr-only" for="repeat_password">Confirm Password</label>
+                    <input type="password" name="repeat_password" placeholder="Repeat Password..." class="form-control">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label class="sr-only" for="male">Sex</label>
+                    <input type="text" name="male%" placeholder="Male or Female..." class="form-control">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label class="sr-only" for="birthday">Birthday</label>
+                    <input type="text" name="birthday%" placeholder="Birthday year-month-day" class="form-control">
+                </div><!-- form-group -->
+                <div class="text-center">
+                    <input type="submit" name="reg" class="btn btn-success" value="Go, registration!">
+                </div>
+            </form>
+            <div class="form-top-sign text-center">
+                <h4>Already have an account?</h4>
+                <hr>
+            </div><!-- form-top-sign -->
+            <div class="text-center">
+                <a class="btn btn-default" href="login">Sign In</a>
+            </div>
+        </div><!-- form-bottom contact-form -->
+    </div><!-- col-sm-4 col-sm-offset-4 form-box -->
+</div><!-- row -->
