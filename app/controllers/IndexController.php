1907e0bb3dea836e422fd07dc20f4c8229540730
VikRR <rubetc.v.i@gmail.com>
2017-03-29 18:05:44 +0300

    add index controller

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,7 +5,7 @@ namespace bwttest\app\controllers;
 
 use bwttest\app\core\Controller;
 
-class MainController extends Controller
+class IndexController extends Controller
 {
     public function __construct()
     {
@@ -14,6 +14,18 @@ class MainController extends Controller
 
     public function index()
     {
+        //$date = '01/01/1979';
+        //echo $date.'<br>';
+        //print $timestamp = strtotime(str_replace('/','-', $date));
+        //echo '<br>';
+        //print date('d/m/Y', $timestamp);
+        //echo '<br>';
+        //echo '<br>';
+
+        //$model = $this->model->load('user');
+        //debug($model->findAll());
+
+
         if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
             header('Location: weather');
         } else {
