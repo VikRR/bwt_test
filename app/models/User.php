73fb96ad7e8c1cf85bed68bfc4a99efed2b17f28
VikRR <rubetc.v.i@gmail.com>
2017-04-01 12:46:49 +0300

    change user model

--- app/models/User.php
+++ app/models/User.php
@@ -4,8 +4,17 @@ namespace bwttest\app\models;
 
 use bwttest\app\core\Model;
 
+/**
+ * Class User
+ * @package bwttest\app\models
+ */
 class User extends Model
 {
+    /**
+     * The name of the table with which the model is associated
+     *
+     * @var string
+     */
     public $table = 'users';
 
     public function __construct()
@@ -13,25 +22,32 @@ class User extends Model
         parent::__construct();
     }
 
-    public function uniqueEmail($data)
-    {
-        $stmt = $this->query("SELECT COUNT(email) as `count` FROM $this->table WHERE email=?", $data);
-
-        $res = $stmt->fetch();
-
-        if($res['count'] == 0){
-
-            return true;
-        }else{
-            throw new \Exception('Email is not unique, please enter another.');
-        }
-    }
-
-    public function insert($params = [])
+    /**
+     * Add user data into table users
+     *
+     * @param array $params
+     * @return \PDOStatement
+     */
+    public function insertUser($params = [])
     {
         $stmt = $this->query("INSERT INTO $this->table (first_name, last_name, email, password, male, birthday) VALUES (:first_name, :last_name, :email, :password, :male, :birthday)",
             $params);
 
         return $stmt;
+    }
+
+    /**
+     * Select user data from the table users when authorizing
+     *
+     * @param $data
+     * @return mixed
+     */
+    public function login($data)
+    {
+        $stmt = $this->query("SELECT * FROM $this->table WHERE email=:email", $data);
+
+        $res = $stmt->fetch();
+
+        return $res;
     }
 }
