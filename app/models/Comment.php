2961336d7d80dd1c0b9da9a86ac0811c3b7ed17f
VikRR <rubetc.v.i@gmail.com>
2017-04-01 12:47:14 +0300

    add comment model

---
+++ app/models/Comment.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace bwttest\app\models;
+
+
+use bwttest\app\core\Model;
+
+/**
+ * Class Comment
+ * @package bwttest\app\models
+ */
+class Comment extends Model
+{
+    /**
+     * The name of the table with which the model is associated
+     *
+     * @var string
+     */
+    public $table = 'comments';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Insert comment into  comments table
+     *
+     * @param $data
+     * @return \PDOStatement
+     */
+    public function insertComment($data)
+    {
+        $stmt = $this->query("INSERT INTO $this->table (user_id, guest_id, comment) VALUES(:user_id, :guest_id, :comment)",
+            $data);
+
+        return $stmt;
+    }
+
+    /**
+     * Select all users comments
+     *
+     * @return array
+     */
+    public function usersComments()
+    {
+        $stmt = $this->pdo->execute('SELECT us.first_name, us.last_name, co.comment, co.date_create FROM comments co, users us WHERE co.user_id = us.id ORDER BY co.date_create');
+
+        $res = $stmt->fetchAll();
+
+        return $res;
+    }
+
+    /**
+     * Select all guests comments
+     *
+     * @return array
+     */
+    public function guestsComments()
+    {
+        $stmt = $this->pdo->execute('SELECT gu.guest, co.comment, co.date_create FROM comments co, guests gu WHERE co.guest_id = gu.id ORDER BY co.date_create');
+
+        $res = $stmt->fetchAll();
+
+        return $res;
+    }
+
+}
