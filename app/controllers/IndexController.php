acc25db6f87935b4c2b67ea266c0f117ffa4786d
VikRR <rubetc.v.i@gmail.com>
2017-04-01 12:44:29 +0300

    change index controller

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,31 +5,29 @@ namespace bwttest\app\controllers;
 
 use bwttest\app\core\Controller;
 
+/**
+ * Class IndexController
+ * @package bwttest\app\controllers
+ */
 class IndexController extends Controller
 {
+    /**
+     * IndexController constructor.
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * Main page
+     */
     public function index()
     {
-        //$date = '01/01/1979';
-        //echo $date.'<br>';
-        //print $timestamp = strtotime(str_replace('/','-', $date));
-        //echo '<br>';
-        //print date('d/m/Y', $timestamp);
-        //echo '<br>';
-        //echo '<br>';
-
-        //$model = $this->model->load('user');
-        //debug($model->findAll());
-
-
-        if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
+        if (!empty($_SESSION['username'])) {
             header('Location: weather');
         } else {
-            $this->view->load('index/index');
+            $this->view->load('form/registration');
         }
     }
 }
