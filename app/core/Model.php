553caa1237d25aa1c2536b25c3dc2f68b70c25bf
VikRR <rubetc.v.i@gmail.com>
2017-04-01 12:46:00 +0300

    change model class

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,9 +2,24 @@
 
 namespace bwttest\app\core;
 
+/**
+ * Class Model
+ * @package bwttest\app\core
+ */
 class Model
 {
-    private $pdo;
+    /**
+     * Object DB
+     *
+     * @var DB
+     */
+    protected $pdo;
+
+    /**
+     * The name of the table with which the model is associated
+     *
+     * @var string
+     */
     public $table;
 
     public function __construct()
@@ -12,15 +27,24 @@ class Model
         $this->pdo = DB::connect();
     }
 
+    /**
+     * Creating a new model object
+     *
+     * @param $name
+     * @return mixed
+     */
     public function load($name)
     {
         $model = 'bwttest\app\models\\' . ucfirst($name);
 
-        //$model .= ucfirst($name);
-
         return new $model();
     }
 
+    /**
+     * @param $sql
+     * @param array $param
+     * @return \PDOStatement
+     */
     public function query($sql, $param = [])
     {
         $stmt = $this->pdo->execute($sql, $param);
@@ -28,18 +52,34 @@ class Model
         return $stmt;
     }
 
+    /**
+     * Select of all data from the table
+     *
+     * @param array $param
+     * @return array
+     */
     public function findAll($param=[])
     {
-        $stmt = $this->query("SELECT * FROM {$this->table}", $param);
+        $stmt = $this->query("SELECT * FROM $this->table", $param);
 
         $res = $stmt->fetchAll();
 
         return $res;
     }
 
-    public function getTable()
+    /**
+     * Check for the presence of email in the table
+     *
+     * @param $data
+     * @return mixed
+     */
+    public function uniqueEmail($data)
     {
-        return $this->table;
+        $stmt = $this->query("SELECT COUNT(email) as `count` FROM $this->table WHERE email=?", $data);
+
+        $res = $stmt->fetch();
+
+        return $res;
     }
 
 }
