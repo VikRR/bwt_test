585530c41c98ebd8b95df632b453bfb036968f78
VikRR <rubetc.v.i@gmail.com>
2017-04-01 12:45:40 +0300

    change controller class

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,12 +2,24 @@
 
 namespace bwttest\app\core;
 
-
+/**
+ * Class Controller
+ * @package bwttest\app\core
+ */
 class Controller
 {
+    /**
+     * @var Model
+     */
     public $model;
+    /**
+     * @var View
+     */
     public $view;
 
+    /**
+     * Controller constructor.
+     */
     public function __construct()
     {
         $this->model = new Model();
