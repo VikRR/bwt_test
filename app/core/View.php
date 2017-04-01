75b8a83d09ecd48d5bf975d64995bf33a7b3ca80
VikRR <rubetc.v.i@gmail.com>
2017-04-01 12:46:34 +0300

    change view class

--- app/core/View.php
+++ app/core/View.php
@@ -2,44 +2,46 @@
 
 namespace bwttest\app\core;
 
-
+/**
+ * Class View
+ * @package bwttest\app\core
+ */
 class View
 {
+    /**
+     * @param $view
+     * @param array $data
+     */
     public function load($view, $data = [])
     {
         include_once ROOT . '/app/views/layouts/header.php';
-        if (!empty($_SESSION['username'])) {
-            include_once ROOT . '/app/views/layouts/menu.php';
-        }
+        include_once ROOT . '/app/views/layouts/menu.php';
         include_once ROOT . '/app/views/' . $view . '.php';
         include_once ROOT . '/app/views/layouts/footer.php';
     }
 
+    /**
+     * Obtaining data from forms using POST or GET method
+     *
+     * @param $method
+     * @return array
+     */
     public function input($method)
     {
         $data = [];
-
-        if($method === 'post'){
-            if(isset($_POST) && !empty($_POST)){
-                foreach ($_POST as $k => $v){
+        if ($method === 'post') {
+            if (isset($_POST) && !empty($_POST)) {
+                foreach ($_POST as $k => $v) {
                     $data[$k] = $v;
                 }
             }
-        }elseif($method === 'get'){
-            if(isset($_GET) && !empty($_GET)){
-                foreach ($_GET as $k => $v){
+        } elseif ($method === 'get') {
+            if (isset($_GET) && !empty($_GET)) {
+                foreach ($_GET as $k => $v) {
                     $data[$k] = $v;
                 }
             }
         }
-
-        //$type = '$_' . strtoupper($method).'[]';
-
-        //if (isset($type) && !empty($type)) {
-        //    foreach ($type as $k => $v) {
-        //        $data[$k] = $v;
-        //    }
-        //}
 
         return $data;
     }
